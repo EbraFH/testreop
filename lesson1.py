f84665483ee9b00836edea29b33345b2a6d70552
EbraFH <102802125+EbraFH@users.noreply.github.com>
2022-12-01 12:19:19 +0200

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+# 
+a = 3
+# b = a
+b = 1.7
+c = "abc"
+d = False  # True
+# student_grade = 90
+
+# Single line comment
+"""
+First comment line
+Second comment line
+Third comment line
+"""
+print(a, b, c,6, d, 1.9, "fdgdfg", True, sep="," ,end="\n")
+print(type(a), type(b), type(c), type(d))
